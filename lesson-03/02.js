5220f7ce0fe2c53d9e4996a6fd046dea37cea2c9
IJTINK7 <ijtink7@mail.ru>
2023-08-19 22:56:29 +0300

    Changing 00-3-2 file

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,3 +1,3 @@
 const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
 
-console.log((Boolean(words.indexOf("апельсин", 0) === -1) ?  console.log("Придется поискать в другом магазине…") : console.log("Ура! нашел")))
+(Boolean(words.indexOf("апельсин", 0) === -1) ?  console.log("Придется поискать в другом магазине…") : console.log("Ура! нашел"))
